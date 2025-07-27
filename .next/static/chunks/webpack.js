267b1d42b2780a48fe1a910f82461e3d3c5a8129
makemoneyzz <makemoneyzzz@proton.me>
2025-07-27 19:24:24 +0000

    ✅ Added TypeScript types for Vercel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bb5eb8402cae9f39"; }
+/******/ 		__webpack_require__.h = function() { return "3efebcd846cbc19b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
